ae4f8d942c074fff94ffef36a6242ff3c36f738d
rainbeam <rainbeam@riseup.net>
2016-05-23 13:19:18 +0100

    separate out splitting functionality

---
+++ contracts/auction_manager.sol
@@ -0,0 +1,255 @@
+import 'erc20/erc20.sol';
+import 'assertive.sol';
+
+contract AuctionManager is Assertive {
+    struct Auction {
+        address beneficiary;
+        ERC20 selling;
+        ERC20 buying;
+        uint min_bid;
+        uint min_increase;
+        uint sell_amount;
+        uint collected;
+        uint COLLECT_MAX;
+        uint claimed;
+        uint expiration;
+        bool reversed;
+        uint excess_claimable;
+    }
+    struct Auctionlet {
+        uint     auction_id;
+        address  last_bidder;
+        uint     last_bid;
+        uint     quantity;
+        bool     unclaimed;
+    }
+
+    uint constant INFINITY = 2 ** 256 - 1;
+
+    mapping(uint => Auction) _auctions;
+    uint _last_auction_id;
+
+    mapping(uint => Auctionlet) _auctionlets;
+    uint _last_auctionlet_id;
+
+    // Create a new auction, with specific parameters.
+    // Bidding is done through the auctions associated auctionlets,
+    // of which there is one initially.
+    function newAuction( address beneficiary
+                        , ERC20 selling
+                        , ERC20 buying
+                        , uint sell_amount
+                        , uint min_bid
+                        , uint min_increase
+                        , uint duration
+                        )
+        returns (uint)
+    {
+        return newTwoWayAuction(beneficiary, selling, buying, sell_amount,
+                                min_bid, min_increase, duration, INFINITY);
+    }
+    function newTwoWayAuction( address beneficiary
+                             , ERC20 selling
+                             , ERC20 buying
+                             , uint sell_amount
+                             , uint min_bid
+                             , uint min_increase
+                             , uint duration
+                             , uint COLLECT_MAX
+                             )
+        returns (uint)
+    {
+        Auction memory a;
+        a.beneficiary = beneficiary;
+        a.selling = selling;
+        a.buying = buying;
+        a.sell_amount = sell_amount;
+        a.min_bid = min_bid;
+        a.min_increase = min_increase;
+        a.expiration = getTime() + duration;
+        a.COLLECT_MAX = COLLECT_MAX;
+
+        var received_lot = selling.transferFrom(beneficiary, this, sell_amount);
+        assert(received_lot);
+
+        _auctions[++_last_auction_id] = a;
+
+        // create the base auctionlet
+        newAuctionlet({auction_id: _last_auction_id,
+                       quantity:    sell_amount});
+
+        return _last_auction_id;
+    }
+    function newAuctionlet(uint auction_id, uint quantity)
+        internal returns (uint)
+    {
+        var A = _auctions[auction_id];
+
+        Auctionlet memory auctionlet;
+        auctionlet.auction_id = auction_id;
+        auctionlet.unclaimed = true;
+
+        if (A.reversed) {
+            auctionlet.last_bid = quantity;
+        } else {
+            auctionlet.quantity = quantity;
+        }
+
+        _auctionlets[++_last_auctionlet_id] = auctionlet;
+
+        return _last_auctionlet_id;
+    }
+    // bid on a specifc auctionlet
+    function bid(uint auctionlet_id, uint bid_how_much) {
+        _assertBiddable(auctionlet_id, bid_how_much);
+        _doBid(auctionlet_id, msg.sender, bid_how_much);
+    }
+    // Parties to an auction can claim their take. The auction creator
+    // (the beneficiary) can claim across an entire auction. Individual
+    // auctionlet high bidders must claim per auctionlet.
+    function claim(uint id) {
+        if (msg.sender == _auctions[id].beneficiary) {
+            _doClaimSeller(id);
+        } else {
+            _doClaimBidder(id, msg.sender);
+        }
+    }
+    // Check whether an auctionlet is eligible for bidding on
+    function _assertBiddable(uint auctionlet_id, uint bid_how_much) internal {
+        var a = _auctionlets[auctionlet_id];
+        var A = _auctions[a.auction_id];
+
+        assert(a.auction_id > 0);  // test for deleted auctionlet
+
+        var expired = A.expiration <= getTime();
+        assert(!expired);
+
+        if (A.reversed) {
+            _assertReverseBiddable(auctionlet_id, bid_how_much);
+        } else {
+            _assertForwardBiddable(auctionlet_id, bid_how_much);
+        }
+    }
+    function _assertForwardBiddable(uint auctionlet_id, uint bid_how_much) internal {
+        var a = _auctionlets[auctionlet_id];
+        var A = _auctions[a.auction_id];
+
+        if (a.last_bidder == 0) {  // check for base auctionlet
+            assert(bid_how_much >= A.min_bid);
+        } else {
+            assert(bid_how_much >= (a.last_bid + A.min_increase));
+        }
+    }
+    function _assertReverseBiddable(uint auctionlet_id, uint bid_how_much) internal {
+        var a = _auctionlets[auctionlet_id];
+        //@log bid how much: `uint bid_how_much`
+        //@log quantity:     `uint a.quantity`
+        //@log last bid:     `uint a.last_bid`
+        assert(bid_how_much <= a.quantity); // TODO: - min_decrease
+    }
+    function _doBid(uint auctionlet_id, address bidder, uint bid_how_much)
+        internal
+    {
+        var a = _auctionlets[auctionlet_id];
+        var A = _auctions[a.auction_id];
+
+        uint receive_amount;
+        if (A.reversed) {
+            receive_amount = a.last_bid;
+        } else {
+            receive_amount = bid_how_much;
+        }
+
+        var received_bid = A.buying.transferFrom(bidder, this, receive_amount);
+        assert(received_bid);
+
+        var returned_bid = A.buying.transfer(a.last_bidder, a.last_bid);
+        assert(returned_bid);
+
+        A.collected += receive_amount;
+        A.collected -= a.last_bid;
+
+        a.last_bidder = bidder;
+
+        if (A.reversed) {
+            A.excess_claimable += a.quantity - bid_how_much;
+            a.quantity = bid_how_much;
+        } else {
+            a.last_bid = bid_how_much;
+        }
+
+        if (!A.reversed && (A.collected >= A.COLLECT_MAX)) {
+            // return excess to bidder
+            var excess = A.collected - A.COLLECT_MAX;
+            var returned_excess = A.buying.transfer(bidder, excess);
+            assert(returned_excess);
+
+            A.collected = A.COLLECT_MAX;
+            A.reversed = true;
+
+            a.last_bid = bid_how_much - excess;
+
+            var effective_target_bid = (a.quantity * A.COLLECT_MAX) / A.sell_amount;
+            var reduced_quantity = (a.quantity * effective_target_bid) / bid_how_much;
+            //@log effective target bid: `uint effective_target_bid`
+            //@log previous quantity:    `uint a.quantity`
+            //@log reduced quantity:     `uint reduced_quantity`
+            a.quantity = reduced_quantity;
+        }
+    }
+    // claim the existing bids from all auctionlets connected to a
+    // specific auction
+    function _doClaimSeller(uint auction_id) internal {
+        var A = _auctions[auction_id];
+
+        //@log collected: `uint A.collected`
+        //@log claimed:   `uint A.claimed`
+        //@log balance:   `uint A.buying.balanceOf(this)`
+        var settled = A.buying.transfer(A.beneficiary, A.collected - A.claimed);
+        assert(settled);
+
+        // transfer excess sell token
+        if (A.reversed) {
+            var settled_excess = A.selling.transfer(A.beneficiary,
+                                                    A.excess_claimable);
+            assert(settled_excess);
+            A.excess_claimable = 0;
+        }
+
+        A.claimed = A.collected;
+    }
+    // claim the proceedings from an auction for the highest bidder
+    function _doClaimBidder(uint auctionlet_id, address claimer) internal {
+        var a = _auctionlets[auctionlet_id];
+        var A = _auctions[a.auction_id];
+
+        assert(claimer == a.last_bidder);
+
+        var expired = A.expiration <= getTime();
+        assert(expired);
+
+        assert(a.unclaimed);
+
+        var settled = A.selling.transfer(a.last_bidder, a.quantity);
+        assert(settled);
+
+        a.unclaimed = false;
+        delete _auctionlets[auctionlet_id];
+    }
+    function getAuction(uint id) constant
+        returns (address, ERC20, ERC20, uint, uint, uint, uint)
+    {
+        Auction a = _auctions[id];
+        return (a.beneficiary, a.selling, a.buying,
+                a.sell_amount, a.min_bid, a.min_increase, a.expiration);
+    }
+    function getAuctionlet(uint id) constant
+        returns (uint, address, uint, uint)
+    {
+        Auctionlet a = _auctionlets[id];
+        return (a.auction_id, a.last_bidder, a.last_bid, a.quantity);
+    }
+    function getTime() public constant returns (uint) {
+        return block.timestamp;
+    }
+}
--- contracts/splitting_auction_test.sol
+++ contracts/splitting_auction_test.sol
@@ -2,8 +2,7 @@ import 'dapple/test.sol';
 import 'erc20/base.sol';
 import 'splitting_auction.sol';
 
-
-contract Manager is SplittableAuctionManager {
+contract TestableSplitManager is SplittableAuctionManager {
     uint public debug_timestamp;
 
     function getTime() public constant returns (uint) {
@@ -14,11 +13,11 @@ contract Manager is SplittableAuctionManager {
     }
 }
 
-contract AuctionTester is Tester {
-    Manager manager;
-    function bindManager(Manager _manager) {
+contract SplitAuctionTester is Tester {
+    TestableSplitManager manager;
+    function bindManager(TestableSplitManager _manager) {
         _target(_manager);
-        manager = Manager(_t);
+        manager = TestableSplitManager(_t);
     }
     function doApprove(address spender, uint value, ERC20 token) {
         token.approve(spender, value);
@@ -38,10 +37,10 @@ contract AuctionTester is Tester {
 }
 
 contract SplittingAuctionManagerTest is Test {
-    Manager manager;
-    AuctionTester seller;
-    AuctionTester bidder1;
-    AuctionTester bidder2;
+    TestableSplitManager manager;
+    SplitAuctionTester seller;
+    SplitAuctionTester bidder1;
+    SplitAuctionTester bidder2;
 
     ERC20 t1;
     ERC20 t2;
@@ -51,7 +50,7 @@ contract SplittingAuctionManagerTest is Test {
     uint constant T2 = 7 ** 10;
 
     function setUp() {
-        manager = new Manager();
+        manager = new TestableSplitManager();
         manager.setTime(block.timestamp);
 
         var million = 10 ** 6;
@@ -59,19 +58,19 @@ contract SplittingAuctionManagerTest is Test {
         t1 = new ERC20Base(million * T1);
         t2 = new ERC20Base(million * T2);
 
-        seller = new AuctionTester();
+        seller = new SplitAuctionTester();
         seller.bindManager(manager);
 
         t1.transfer(seller, 200 * T1);
         seller.doApprove(manager, 200 * T1, t1);
 
-        bidder1 = new AuctionTester();
+        bidder1 = new SplitAuctionTester();
         bidder1.bindManager(manager);
 
         t2.transfer(bidder1, 1000 * T2);
         bidder1.doApprove(manager, 1000 * T2, t2);
 
-        bidder2 = new AuctionTester();
+        bidder2 = new SplitAuctionTester();
         bidder2.bindManager(manager);
 
         t2.transfer(bidder2, 1000 * T2);
@@ -80,267 +79,6 @@ contract SplittingAuctionManagerTest is Test {
     function testSetUp() {
         assertEq(t2.balanceOf(bidder1), 1000 * T2);
         assertEq(t2.allowance(bidder1, manager), 1000 * T2);
-    }
-    function testNewAuction() {
-        var balance_before = t1.balanceOf(seller);
-        var id = manager.newAuction(seller,  // beneficiary
-                                    t1,      // selling
-                                    t2,      // buying
-                                    100 * T1,// sell amount (t1)
-                                    0 * T2,  // minimum bid (t2)
-                                    1 * T2,  // minimum increase
-                                    1 years  // duration
-                                   );
-        assertEq(id, 1);
-        var balance_after = t1.balanceOf(seller);
-
-        var (beneficiary, selling, buying,
-             sell_amount, min_bid, min_increase, expiration) = manager.getAuction(id);
-
-        assertEq(beneficiary, seller);
-        assertTrue(selling == t1);
-        assertTrue(buying == t2);
-        assertEq(sell_amount, 100 * T1);
-        assertEq(min_bid, 0 * T2);
-        assertEq(min_increase, 1 * T2);
-        assertEq(expiration, manager.getTime() + 1 years);
-
-        var balance_diff = balance_before - balance_after;
-        assertEq(balance_diff, 100 * T1);
-    }
-    function testNewAuctionlet() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 0 * T2, 1 * T2, 1 years);
-
-        // can't always know what the auctionlet id is as it is
-        // only an internal type. But for the case of a single auction
-        // there should be a single auctionlet created with id 1.
-        var (auction_id, last_bidder1,
-             last_bid, quantity) = manager.getAuctionlet(1);
-
-        assertEq(auction_id, id);
-        assertEq(last_bidder1, 0);
-        assertEq(last_bid, 0 * T2);
-        assertEq(quantity, 100 * T1);
-    }
-    function testFailBidUnderMinBid() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 9 * T2);
-    }
-    function testFailBidUnderMinIncrease() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 2 * T2, 1 years);
-        bidder1.doBid(1, 10 * T2);
-        bidder2.doBid(1, 11 * T2);
-    }
-    function testBid() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 11 * T2);
-
-        var (auction_id, last_bidder1,
-             last_bid, quantity) = manager.getAuctionlet(1);
-
-        assertEq(last_bidder1, bidder1);
-        assertEq(last_bid, 11 * T2);
-    }
-    function testFailBidTransfer() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-
-        // this should throw as bidder1 only has 1000 t2
-        bidder1.doBid(1, 1001 * T2);
-    }
-    function testBidTransfer() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-
-        var bidder1_t2_balance_before = t2.balanceOf(bidder1);
-        bidder1.doBid(1, 11 * T2);
-        var bidder1_t2_balance_after = t2.balanceOf(bidder1);
-
-        var balance_diff = bidder1_t2_balance_before - bidder1_t2_balance_after;
-        assertEq(balance_diff, 11 * T2);
-    }
-    function testBidReturnsToPrevBidder() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-
-        var bidder1_t2_balance_before = t2.balanceOf(bidder1);
-        var manager_t2_balance_before = t2.balanceOf(manager);
-        bidder1.doBid(1, 11 * T2);
-        bidder2.doBid(1, 12 * T2);
-        var bidder1_t2_balance_after = t2.balanceOf(bidder1);
-        var manager_t2_balance_after = t2.balanceOf(manager);
-
-        var bidder_balance_diff = bidder1_t2_balance_before - bidder1_t2_balance_after;
-        var manager_balance_diff = manager_t2_balance_after - manager_t2_balance_before;
-        assertEq(bidder_balance_diff, 0 * T2);
-        assertEq(manager_balance_diff, 12 * T2);
-    }
-    function testFailBidExpired() {
-        manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 11 * T2);
-
-        // force expiry
-        manager.setTime(manager.getTime() + 2 years);
-
-        bidder2.doBid(1, 12 * T2);
-    }
-    function testClaimTransfersBenefactor() {
-        var seller_t2_balance_before = t2.balanceOf(seller);
-        var seller_t1_balance_before = t1.balanceOf(seller);
-
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 40 * T2);
-        seller.doClaim(id);
-
-        var seller_t2_balance_after = t2.balanceOf(seller);
-        var seller_t1_balance_after = t1.balanceOf(seller);
-
-        var diff_t1 = seller_t1_balance_before - seller_t1_balance_after;
-        var diff_t2 = seller_t2_balance_after - seller_t2_balance_before;
-
-        assertEq(diff_t2, 40 * T2);
-        assertEq(diff_t1, 100 * T1);
-    }
-    function testClaimAfterDoubleBid() {
-        var seller_t2_balance_before = t2.balanceOf(seller);
-        var seller_t1_balance_before = t1.balanceOf(seller);
-
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 40 * T2);
-        bidder2.doBid(1, 60 * T2);
-        seller.doClaim(id);
-
-        var seller_t2_balance_after = t2.balanceOf(seller);
-        var seller_t1_balance_after = t1.balanceOf(seller);
-
-        var diff_t1 = seller_t1_balance_before - seller_t1_balance_after;
-        var diff_t2 = seller_t2_balance_after - seller_t2_balance_before;
-
-        assertEq(diff_t2, 60 * T2);
-        assertEq(diff_t1, 100 * T1);
-    }
-    function testBenefactorClaimLogged() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 11 * T2);
-        seller.doClaim(id);
-
-        var seller_t1_balance_before = t1.balanceOf(seller);
-        var seller_t2_balance_before = t2.balanceOf(seller);
-
-        // calling claim again should not do anything as there
-        // have been no new bids
-        seller.doClaim(id);
-
-        var seller_t1_balance_after = t1.balanceOf(seller);
-        var seller_t2_balance_after = t2.balanceOf(seller);
-
-        var diff_t1 = seller_t1_balance_before - seller_t1_balance_after;
-        var diff_t2 = seller_t2_balance_after - seller_t2_balance_before;
-
-        assertEq(diff_t2, 0);
-        assertEq(diff_t1, 0);
-    }
-    function testClaimTransfersBidder() {
-        var bidder_t2_balance_before = t2.balanceOf(bidder1);
-        var bidder_t1_balance_before = t1.balanceOf(bidder1);
-
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 11 * T2);
-
-        // force expiry
-        manager.setTime(manager.getTime() + 2 years);
-
-        bidder1.doClaim(1);
-
-        var bidder_t2_balance_after = t2.balanceOf(bidder1);
-        var bidder_t1_balance_after = t1.balanceOf(bidder1);
-
-        var diff_t1 = bidder_t1_balance_after - bidder_t1_balance_before;
-        var diff_t2 = bidder_t2_balance_before - bidder_t2_balance_after;
-
-        assertEq(diff_t2, 11 * T2);
-        assertEq(diff_t1, 100 * T1);
-    }
-    function testFailClaimNonParty() {
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 11 * T2);
-        // bidder2 is not party to the auction and should not be able to
-        // initiate a claim
-        bidder2.doClaim(1);
-    }
-    function testFailClaimProceedingsPreExpiration() {
-        // bidders cannot claim their auctionlet until the auction has
-        // expired.
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(1, 11 * T2);
-        bidder1.doClaim(1);
-    }
-    function testMultipleNewAuctions() {
-        // auction manager should be able to manage multiple auctions
-        t2.transfer(seller, 200 * T2);
-        seller.doApprove(manager, 200 * T2, t2);
-
-        var t1_balance_before = t1.balanceOf(seller);
-        var t2_balance_before = t2.balanceOf(seller);
-
-        var id1 = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        var id2 = manager.newAuction(seller, t2, t1, 100 * T2, 10 * T1, 1 * T1, 1 years);
-
-        var t1_balance_after = t1.balanceOf(seller);
-        var t2_balance_after = t2.balanceOf(seller);
-
-        assertEq(id1, 1);
-        assertEq(id2, 2);
-
-        var t1_balance_diff = t1_balance_before - t1_balance_after;
-        var t2_balance_diff = t2_balance_before - t2_balance_after;
-
-        assertEq(t1_balance_diff, 100 * T1);
-        assertEq(t2_balance_diff, 100 * T2);
-
-        var (beneficiary, selling, buying,
-             sell_amount, min_bid, min_increase, expiration) = manager.getAuction(id2);
-
-        assertEq(beneficiary, seller);
-        assertTrue(selling == t2);
-        assertTrue(buying == t1);
-        assertEq(sell_amount, 100 * T2);
-        assertEq(min_bid, 10 * T1);
-        assertEq(min_increase, 1 * T1);
-        assertEq(expiration, manager.getTime() + 1 years);
-    }
-    function testMultipleAuctionClaims() {
-        manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-
-        var seller_t2_balance_before = t2.balanceOf(seller);
-        var seller_t1_balance_before = t1.balanceOf(seller);
-
-        var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);
-        bidder1.doBid(2, 11 * T2);
-        seller.doClaim(id);
-
-        var seller_t2_balance_after = t2.balanceOf(seller);
-        var seller_t1_balance_after = t1.balanceOf(seller);
-
-        var diff_t1 = seller_t1_balance_before - seller_t1_balance_after;
-        var diff_t2 = seller_t2_balance_after - seller_t2_balance_before;
-
-        assertEq(diff_t2, 11 * T2);
-        assertEq(diff_t1, 100 * T1);
-    }
-    function testFailBidderClaimAgain() {
-        // bidders should not be able to claim their auctionlet more than once
-
-        // create an auction that expires immediately
-        var id1 = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 0 years);
-        var id2 = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 0 years);
-
-        // create bids on two different auctions so that the manager has
-        // enough funds for us to attempt to withdraw all at once
-        bidder1.doBid(1, 11 * T2);
-        bidder2.doBid(2, 11 * T2);
-
-        // now attempt to claim the proceedings from the first
-        // auctionlet twice
-        bidder1.doClaim(1);
-        bidder1.doClaim(1);
     }
     function testSplitBase() {
         var id = manager.newAuction(seller, t1, t2, 100 * T1, 10 * T2, 1 * T2, 1 years);

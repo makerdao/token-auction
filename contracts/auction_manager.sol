01a11ea265802a8d45a356996ecd66fa13b7741c
rain <rainbeam@riseup.net>
2016-06-07 19:45:34 +0100

    use sum directly

--- contracts/auction_manager.sol
+++ contracts/auction_manager.sol
@@ -445,7 +445,7 @@ contract AuctionManager is AuctionUser, EventfulManager {
     function _checkPayouts(Auction A) internal {
         assert(A.beneficiaries.length == A.payouts.length);
         if (!A.reversed) assert(A.payouts[0] >= A.start_bid);
-        assert(cumsum(A.payouts)[A.payouts.length -1] == A.collection_limit);
+        assert(sum(A.payouts) == A.collection_limit);
     }
     function _newTwoWayAuction( address creator
                               , address[] beneficiaries

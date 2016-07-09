20e7cfa1b137db79cd1a59fe6cca5fc4241472cc
rain <rainbeam@riseup.net>
2016-07-09 20:43:35 +0100

    estimate gas costs for reverse auction

--- contracts/tests/gas.sol
+++ contracts/tests/gas.sol
@@ -65,3 +65,68 @@ contract ForwardGasTest is AuctionTest {
         new SplittingAuctionManager();
     }
 }
+
+contract ReverseGasTest is AuctionTest {
+    uint id;
+    uint base;
+
+    modifier pre_create {
+        (id, base) = newAuction();
+        _
+    }
+    modifier pre_bid(uint how_much) {
+        bidder1.doBid(base, how_much);
+        _
+    }
+    modifier force_expiry {
+        manager.addTime(100 years);
+        _
+    }
+    function newAuction() returns (uint, uint) {
+        return manager.newReverseAuction(beneficiary1, t1, t2, 100 * T1, 100 * T2, 1, 1 hours);
+    }
+    function testBaseBid()
+        pre_create
+        logs_gas
+    {
+        bidder1.doBid(base, 90 * T1);
+    }
+    function testBaseSplit()
+        pre_create
+        logs_gas
+    {
+        bidder1.doBid(base, 10 * T1, 50 * T2);
+    }
+    function testSubsequentBid()
+        pre_create
+        pre_bid(50 * T1)
+        logs_gas
+    {
+        bidder1.doBid(base, 10 * T1);
+    }
+    function testSubsequentSplit()
+        pre_create
+        pre_bid(50 * T1)
+        logs_gas
+    {
+        bidder1.doBid(base, 10 * T1, 50 * T2);
+    }
+    function testClaim()
+        pre_create
+        pre_bid(50 * T1)
+        force_expiry
+        logs_gas
+    {
+        bidder1.doClaim(id);
+    }
+    function testNewAuction()
+        logs_gas
+    {
+        newAuction();
+    }
+    function testNewManager()
+        logs_gas
+    {
+        new SplittingAuctionManager();
+    }
+}
